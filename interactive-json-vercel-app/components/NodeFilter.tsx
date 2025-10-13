bdb4b02825f2899fa157f8eeb45965205cc587da
achal7337 <achalgandhip16@gmail.com>
2025-10-13 11:00:38 -0400

    new

---
+++ interactive-json-vercel-app/components/NodeFilter.tsx
@@ -0,0 +1,10 @@
+// components/NodeFilter.tsx
+'use client';
+
+export default function NodeFilter() {
+  return (
+    <div style={{ padding: 8, border: '1px dashed gray' }}>
+      <p>NodeFilter placeholder component</p>
+    </div>
+  );
+}
